2a1c1e3a067ccce1ac43ab2bcfcbd6fd00a36e28
RafaelOsiro <rafaelosiro@hotmail.com>
2021-11-16 10:01:23 -0300

    First commit of the project

---
+++ L4E5.c
@@ -0,0 +1,16 @@
+/*
+UNIVERSIDADE CATÓLICA DE BRASÍLIA
+CURSO: ANÁLISE E DESENVOLVIMENTO DE SISTEMAS
+DISCIPLINA: PROGRAMAÇÃO ESTRUTURADA
+DOCENTE: JOYCE SIQUEIRA
+DISCENTE: RAFAEL RIKI OGAWA OSIRO (UC21100716)
+
+Exercício: 4. Considerando a estrutura abaixo para representar um vetor, implemente um programa que calcule a 
+soma de dois vetores, sabendo que: A + B = < (a1 + a2), (b1 + b2), (c1 + c2) >
+struct vetor{
+int a;
+int b;
+int c;
+};
+*/
+
